35c0fa1e07e2c69e15a26ce23d5d9b824d95f06a
Erycles da Silva Santos <erycles.santos@aditec.local>
2023-05-26 11:48:05 -0300

    realizando estilizações de formulário

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,17 +1,13 @@
+import Input from '../form/Input'
+import Select from '../form/Select'
+import styles from './ProjectForm.module.css'
+
 function ProjectForm(){
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select>
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
+        <form className={styles.form}>
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+            <Input type="number" text="Orçamento do projeto" name="name" placeholder="Insira o orçamento do projeto" />
+            <Select name="category_id" text="Selecione a categoria" />
             <div>
                 <input type="submit" value="Criar projeto" />
             </div>
